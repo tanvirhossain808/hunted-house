12bc07474b92070e169d504644e3de0d895c68c0
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-02-24 00:14:24 +0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,171 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+/**
+ * House
+ */
+const house = new THREE.Group()
+scene.add(house)
+
+//walls
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4, 2.5, 4),
+    new THREE.MeshStandardMaterial({ color: "#acBeB2" })
+)
+walls.position.y = 2.5 / 2
+house.add(walls)
+
+//roof
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, 1, 4),
+    new THREE.MeshStandardMaterial({ color: "#b35f45" })
+)
+house.add(roof)
+roof.position.y = 3
+roof.rotation.y = -Math.PI * .25
+// Temporary sphere
+
+
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 32, 32),
+    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+)
+sphere.position.y = 1
+// scene.add(sphere)
+
+// Floor
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial({ color: '#a9c388' })
+)
+
+//door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, 2),
+    new THREE.MeshStandardMaterial({ color: "#aa7b7b" })
+)
+door.position.z = 2.01
+door.position.y = 1
+house.add(door)
+
+floor.rotation.x = - Math.PI * 0.5
+floor.position.y = 0
+scene.add(floor)
+//bushes
+
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: "#89c854" })
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(.5, .5, .5)
+bush1.position.set(.8, .2, 2.2)
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(.25, .25, .25)
+bush2.position.set(1.4, .1, 2.1)
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(.4, .4, .4)
+bush3.position.set(-.8, .1, 2.2)
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(.15, .15, .15)
+bush4.position.set(-1, .05, 2.6)
+house.add(bush1, bush2, bush3, bush4)
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+scene.add(ambientLight)
+
+// Directional light
+const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+moonLight.position.set(4, 5, - 2)
+gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
+scene.add(moonLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
